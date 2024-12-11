9cf5c7c1cd2f6528a31ca52dbd54957c9760edf0
Thomas-Preat <preatthomas@gmail.com>
2024-12-11 18:53:21 +0100

    Update database.py

--- app/database.py
+++ app/database.py
@@ -27,3 +27,12 @@ def delete_item_by_id(db_conn, item_id):
     cursor.execute("DELETE FROM inventory WHERE id = ?", (item_id,))
     db_conn.commit()
     return cursor.rowcount  # Retourne le nombre de lignes affectées
+
+def add_product(db_conn, name, quantity, price, category):
+    """Ajoute un produit dans la table inventory"""
+    cursor = db_conn.cursor()
+    cursor.execute('''
+        INSERT INTO inventory (name, quantity, price, category)
+        VALUES (?, ?, ?, ?)
+    ''', (name, quantity, price, category))
+    db_conn.commit()
