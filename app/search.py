01f23555089c5c3ced384fe35db81085a8388816
Thomas-Preat <preatthomas@gmail.com>
2024-12-11 18:58:54 +0100

    Update search.py

--- app/search.py
+++ app/search.py
@@ -1,16 +1,18 @@
 def search_products(db_conn, search_criteria):
     cursor = db_conn.cursor()
-    query = "SELECT * FROM inventory WHERE "
-    conditions = []
+    query = "SELECT * FROM inventory"
     values = []
 
-    for criteria in search_criteria:
-        if ":" in criteria:
-            key, value = criteria.split(":")
-            conditions.append(f"{key} LIKE ?")
-            values.append(f"%{value}%")
-    
-    query += " AND ".join(conditions)
+    if search_criteria:  # Si des critères de recherche sont fournis
+        query += " WHERE "
+        conditions = []
+        for criteria in search_criteria:
+            if ":" in criteria:
+                key, value = criteria.split(":")
+                conditions.append(f"{key} LIKE ?")
+                values.append(f"%{value}%")
+        query += " AND ".join(conditions)
+
     cursor.execute(query, values)
     results = cursor.fetchall()
 
