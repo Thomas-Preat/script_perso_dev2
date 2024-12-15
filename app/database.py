d2efab06011b236d0d01beb299b3a9e627e0e9c8
Thomas-Preat <preatthomas@gmail.com>
2024-12-15 16:22:31 +0100

    PEP8 database
    Realise par Thomas Preat
    Mise en forme PEP8 pour le fichier database.py

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def initialize_database(db_path="inventory.db"):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -15,6 +16,7 @@ def initialize_database(db_path="inventory.db"):
     conn.commit()
     return conn
 
+
 def display_all_data(db_conn):
     cursor = db_conn.cursor()
     cursor.execute("SELECT * FROM inventory")
@@ -22,11 +24,13 @@ def display_all_data(db_conn):
     for row in rows:
         print(row)
 
+
 def delete_item_by_id(db_conn, item_id):
     cursor = db_conn.cursor()
     cursor.execute("DELETE FROM inventory WHERE id = ?", (item_id,))
     db_conn.commit()
     return cursor.rowcount  # Retourne le nombre de lignes affectées
+
 
 def add_product(db_conn, name, quantity, price, category):
     """Ajoute un produit dans la table inventory"""
