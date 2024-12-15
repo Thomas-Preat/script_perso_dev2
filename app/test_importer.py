b64dae272e9dfa6a66b6b6021aa46820315a34b0
Thomas-Preat <preatthomas@gmail.com>
2024-12-15 16:21:04 +0100

    PEP8 Tests
    Realise par Thomas Preat
    Mise en former PEP8 pour les tsts unitaires

--- app/test_importer.py
+++ app/test_importer.py
@@ -1,13 +1,15 @@
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import patch
 import os
 import io
 import sqlite3
-import sys
 import csv
 
 from importer import import_csv_files
-from database import initialize_database, display_all_data, delete_item_by_id, add_product
+from database import (
+    initialize_database, display_all_data,
+    delete_item_by_id, add_product
+)
 from search import search_products
 from report import generate_summary_report
 
@@ -40,36 +42,47 @@ class TestImporter(unittest.TestCase):
         ''')
         self.conn.commit()
 
-    def test_import_csv_files(self):
-        """Test d'importation des fichiers CSV"""
-        test_data = "name,quantity,price,category\nApple,10,1.2,Fruit\nBanana,20,0.8,Fruit"
-        with open("test.csv", "w") as f:
-            f.write(test_data)
-
-        # Passer la connexion à la base de données à la fonction import_csv_files
-        import_csv_files(["test.csv"], self.conn)
-
-        # Vérifier l'insertion des données
-        self.cursor.execute("SELECT * FROM inventory WHERE name='Apple'")
-        result = self.cursor.fetchone()
-        self.assertIsNotNone(result)
-        self.assertEqual(result[1], 'Apple')
-
-        os.remove("test.csv")
-
     def test_initialize_database(self):
         """Test de l'initialisation de la base de données"""
-        # Remplacer la connexion en mémoire par un chemin de fichier pour l'initialisation de la DB
+        # Remplacer la connexion en mémoire par un chemin de fichier
+        # pour l'initialisation de la DB
         db_path = ":memory:"  # ou un chemin de fichier valide
         initialize_database(db_path)
 
         # Vérifier que la table 'inventory' existe
         cursor = self.conn.cursor()
-        cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='inventory'")
+        cursor.execute(
+            "SELECT name FROM sqlite_master" +
+            " WHERE type='table' AND name='inventory'"
+            )
         result = cursor.fetchone()
 
         # Le test passera si la table 'inventory' existe
         self.assertIsNotNone(result, "La table 'inventory' n'a pas été créée.")
+
+    def test_import_csv_files(self):
+        """Test d'importation des fichiers CSV"""
+        test_data = (
+            "name,quantity,price,category\n"
+            "Apple,10,1.2,Fruit\nBanana,20,0.8,Fruit"
+            )
+        with open("test.csv", "w") as f:
+            f.write(test_data)
+
+        # Passer la connexion à la base de données
+        # à la fonction import_csv_files
+        import_csv_files(["test.csv"], self.conn)
+
+        # Vérifier l'insertion des données
+        self.cursor.execute("SELECT * FROM inventory WHERE name='Apple'")
+        result = self.cursor.fetchone()
+        print("DEBUG: Result after insertion:", result)  # Debugging line
+        # Vérifier si le produit "Apple" est trouvé
+        self.assertIsNotNone(result)
+        # Vérifier que le nom correspond à "Apple"
+        self.assertEqual(result[1], 'Apple')
+
+        os.remove("test.csv")
 
     def test_add_product(self):
         """Test de l'ajout d'un produit"""
@@ -108,7 +121,8 @@ class TestImporter(unittest.TestCase):
         # Vérifier que le produit n'est plus dans la base de données
         cursor.execute("SELECT * FROM inventory WHERE name='Banana'")
         result_after_delete = cursor.fetchone()
-        self.assertIsNone(result_after_delete)  # Le produit ne doit plus exister
+        # Le produit ne doit plus exister
+        self.assertIsNone(result_after_delete)
 
     def test_display_all_data(self):
         """Test de l'affichage de toutes les données"""
@@ -123,8 +137,11 @@ class TestImporter(unittest.TestCase):
 
             # Vérifier que les produits sont affichés correctement
             output = captured_output.getvalue()
-            self.assertIn("Apple", output)  # Vérifier que "Apple" est dans l'affichage
-            self.assertIn("Banana", output)  # Vérifier que "Banana" est dans l'affichage
+            # Vérifier que "Apple" est dans l'affichage
+            self.assertIn("Apple", output)
+            # Vérifier que "Banana" est dans l'affichage
+            self.assertIn("Banana", output)
+
 
 class TestReport(unittest.TestCase):
 
@@ -171,7 +188,8 @@ class TestReport(unittest.TestCase):
         # Appeler la fonction qui génère le rapport
         generate_summary_report(self.conn, "test_file.csv")
 
-        # Vérifier que le fichier "test_file.csv" existe bien après la génération
+        # Vérifier que le fichier "test_file.csv"
+        # existe bien après la génération
         self.assertTrue(os.path.exists("test_file.csv"))
 
         # Vérifier le contenu du fichier CSV
@@ -180,18 +198,24 @@ class TestReport(unittest.TestCase):
             rows = list(reader)
 
             # Vérifier les en-têtes
-            self.assertEqual(rows[0], ["Category", "Number of Products", "Total Quantity", "Total Value"])
+            self.assertEqual(
+                rows[0], ["Category", "Number of Products",
+                          "Total Quantity", "Total Value"]
+                )
 
             # Vérifier les données du rapport
             self.assertEqual(rows[1], ['Fruit', '2', '30', '28.0'])
             self.assertEqual(rows[2], ['Vegetable', '2', '45', '30.0'])
 
     def test_generate_summary_report_with_no_data(self):
-        """Test de la génération du rapport lorsque la base de données est vide"""
-        # Appeler la fonction qui génère le rapport (aucune donnée dans la table)
+        """Test de la génération du rapport
+        lorsque la base de données est vide"""
+        # Appeler la fonction qui génère le rapport
+        # (aucune donnée dans la table)
         generate_summary_report(self.conn, "test_file.csv")
 
-        # Vérifier que le fichier "test_file.csv" existe bien après la génération
+        # Vérifier que le fichier "test_file.csv"
+        # existe bien après la génération
         self.assertTrue(os.path.exists("test_file.csv"))
 
         # Vérifier le contenu du fichier CSV
@@ -200,10 +224,13 @@ class TestReport(unittest.TestCase):
             rows = list(reader)
 
             # Vérifier que seule la ligne d'en-têtes est présente
-            self.assertEqual(rows, [["Category", "Number of Products", "Total Quantity", "Total Value"]])
+            self.assertEqual(
+                rows, [["Category", "Number of Products",
+                        "Total Quantity", "Total Value"]]
+                )
 
     def test_generate_summary_report_empty_categories(self):
-        """Test de la génération du rapport si certaines catégories sont vides"""
+        """Test génération du rapport si certaines catégories sont vides"""
         # Ajouter des produits dans une seule catégorie
         self.cursor.executemany('''
             INSERT INTO inventory (name, quantity, price, category)
@@ -218,7 +245,8 @@ class TestReport(unittest.TestCase):
         # Appeler la fonction qui génère le rapport
         generate_summary_report(self.conn, "test_file.csv")
 
-        # Vérifier que le fichier "test_file.csv" existe bien après la génération
+        # Vérifier que le fichier "test_file.csv"
+        # existe bien après la génération
         self.assertTrue(os.path.exists("test_file.csv"))
 
         # Vérifier le contenu du fichier CSV
@@ -227,11 +255,15 @@ class TestReport(unittest.TestCase):
             rows = list(reader)
 
             # Vérifier les en-têtes
-            self.assertEqual(rows[0], ["Category", "Number of Products", "Total Quantity", "Total Value"])
+            self.assertEqual(
+                rows[0], ["Category", "Number of Products",
+                          "Total Quantity", "Total Value"]
+                )
 
             # Vérifier les données du rapport
             self.assertEqual(rows[1], ['Fruit', '2', '30', '28.0'])
             self.assertEqual(rows[2], ['Vegetable', '1', '30', '15.0'])
+
 
 class TestSearchProducts(unittest.TestCase):
 
@@ -273,7 +305,8 @@ class TestSearchProducts(unittest.TestCase):
         ])
         self.conn.commit()
 
-        # Appeler la fonction avec un critère de recherche (par exemple, "name:Apple")
+        # Appeler la fonction avec un critère de recherche
+        # (par exemple, "name:Apple")
         search_criteria = ["name:Apple"]
         with patch('sys.stdout', new_callable=io.StringIO) as captured_output:
             search_products(self.conn, search_criteria)
@@ -302,9 +335,13 @@ class TestSearchProducts(unittest.TestCase):
         # Vérification que les produits sont insérés
         self.cursor.execute("SELECT * FROM inventory")
         all_products = self.cursor.fetchall()
-        self.assertEqual(len(all_products), 4, "Les produits n'ont pas été correctement insérés.")
+        self.assertEqual(
+            len(all_products), 4,
+            "Les produits n'ont pas été correctement insérés."
+            )
 
-        # Appeler la fonction avec plusieurs critères de recherche (par exemple, "name:Apple" et "category:Fruit")
+        # Appeler la fonction avec plusieurs critères de recherche
+        # (par exemple, "name:Apple" et "category:Fruit")
         search_criteria = ["name:Apple", "category:Fruit"]
         with patch('sys.stdout', new_callable=io.StringIO) as captured_output:
             search_products(self.conn, search_criteria)
@@ -317,7 +354,7 @@ class TestSearchProducts(unittest.TestCase):
             self.assertNotIn("Broccoli", output)
 
     def test_search_products_with_no_results(self):
-        """Test de recherche avec des critères ne correspondant à aucun produit"""
+        """Test de recherche de critères ne correspondant à aucun produit"""
         # Ajouter des produits à la base de données
         self.cursor.executemany('''
             INSERT INTO inventory (name, quantity, price, category)
@@ -330,14 +367,16 @@ class TestSearchProducts(unittest.TestCase):
         ])
         self.conn.commit()
 
-        # Appeler la fonction avec des critères qui ne correspondent à aucun produit
+        # Appeler la fonction avec des critères
+        # qui ne correspondent à aucun produit
         search_criteria = ["name:Orange"]
         with patch('sys.stdout', new_callable=io.StringIO) as captured_output:
             search_products(self.conn, search_criteria)
 
             # Vérifier qu'aucun résultat n'est trouvé
             output = captured_output.getvalue().strip()
-            self.assertEqual(output, "")  # Aucun résultat trouvé, donc sortie vide
+            # Aucun résultat trouvé, donc sortie vide
+            self.assertEqual(output, "")
 
     def test_search_products_with_wildcards(self):
         """Test de recherche avec des critères incluant des jokers"""
@@ -353,7 +392,8 @@ class TestSearchProducts(unittest.TestCase):
         ])
         self.conn.commit()
 
-        # Appeler la fonction avec un critère incluant un joker, par exemple "name:Ap"
+        # Appeler la fonction avec un critère incluant un joker
+        # par exemple "name:Ap"
         search_criteria = ["name:Ap"]
         with patch('sys.stdout', new_callable=io.StringIO) as captured_output:
             search_products(self.conn, search_criteria)
@@ -384,12 +424,14 @@ class TestSearchProducts(unittest.TestCase):
         with patch('sys.stdout', new_callable=io.StringIO) as captured_output:
             search_products(self.conn, search_criteria)
 
-            # Vérifier que tous les produits sont trouvés (aucun critère de recherche)
+            # Vérifier que tous les produits sont trouvés
+            # (aucun critère de recherche)
             output = captured_output.getvalue().strip()
             self.assertIn("Apple", output)
             self.assertIn("Banana", output)
             self.assertIn("Carrot", output)
             self.assertIn("Broccoli", output)
+
 
 if __name__ == "__main__":
     unittest.main()
